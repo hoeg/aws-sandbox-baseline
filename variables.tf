d1ce7d4db2ebb75ae4d31c3e7ae507541e2e9d5c
hoeg <peter.steffensen@gmail.com>
2018-12-27 14:52:22 +0100

    added username

--- variables.tf
+++ variables.tf
@@ -6,6 +6,10 @@ variable "max-spend" {
   description = "Maximum amount to spend on the accoung pr. month."
 }
 
-varialbe "cloudtrail-bucket-name" {
+variable "cloudtrail-bucket-name" {
   description = "Name of the bucket holding the cloudtrail logs."
+}
+
+variable "username" {
+  description = "Name of the admin user created"
 }
