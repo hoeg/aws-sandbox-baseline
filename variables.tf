c415b63a0e23ffd6e11056d4a6cc350d5fac388e
hoeg <peter.steffensen@gmail.com>
2018-12-27 14:05:26 +0100

    addewd variables file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "Region to deploy resources in."
+}
+
+variable "max-spend" {
+  description = "Maximum amount to spend on the accoung pr. month."
+}
+
+varialbe "cloudtrail-bucket-name" {
+  description = "Name of the bucket holding the cloudtrail logs."
+}
